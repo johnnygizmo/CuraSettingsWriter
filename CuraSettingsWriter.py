7960e80a6066e5659d063b283aa64eb14a7f8b5e
Johnny Matthews <johnny.matthews@gmail.com>
2018-06-06 21:38:37 -0500

    Create CuraSettingsWriter.py

---
+++ CuraSettingsWriter.py
@@ -0,0 +1,76 @@
+from UM.Application import Application
+from UM.Preferences import Preferences
+from UM.Settings.ContainerRegistry import ContainerRegistry
+from UM.Workspace.WorkspaceWriter import WorkspaceWriter
+
+
+import UM.Settings.SettingRelation
+
+class CuraSettingsWriter(WorkspaceWriter):
+   
+    def write(self, stream, nodes, mode):
+        stream.write("<style>")
+        stream.write(" .category { font-size:1.5em; } ")
+        stream.write(" .off { background-color:grey; } ")
+        stream.write("</style>")
+        
+        application = Application.getInstance()
+        machine_manager = application.getMachineManager()        
+        stack = application.getGlobalContainerStack()
+
+        global_stack = machine_manager.activeMachine
+
+        stream.write("<table width=50% border=1 cellpadding=3>")
+
+        self._doTree(global_stack,"resolution",stream,0)
+        self._doTree(global_stack,"shell",stream,0)
+        self._doTree(global_stack,"infill",stream,0)
+        self._doTree(global_stack,"material",stream,0)
+        self._doTree(global_stack,"speed",stream,0)
+        self._doTree(global_stack,"travel",stream,0)
+
+        self._doTree(global_stack,"cooling",stream,0)
+        self._doTree(global_stack,"support",stream,0)
+        self._doTree(global_stack,"platform_adhesion",stream,0)
+        self._doTree(global_stack,"meshfix",stream,0)
+        self._doTree(global_stack,"blackmagic",stream,0)
+        self._doTree(global_stack,"experimental",stream,0)
+        self._doTree(global_stack,"dual",stream,0)
+        self._doTree(global_stack,"machine_settings",stream,0)
+
+        # for key in global_stack.getAllKeys():
+        #     if global_stack.getProperty(key,"enabled") == True:
+        #         if global_stack.getProperty(key,"type") == "category":
+        #             self._doTree(global_stack,key,stream,0)
+
+        stream.write("</table>")
+        return True
+
+
+
+    def _doTree(self,stack,key,stream,depth):   
+        #output node
+        if stack.getProperty(key,"type") == "category":
+            stream.write("<tr>")
+            stream.write("<td class=category colspan=3>" + str(stack.getProperty(key,"label")) + "</td>")
+            #stream.write("<td class=category>" + str(key) + "</td>")
+            stream.write("</tr>")
+        else:
+            style = "ok"    
+            if stack.getProperty(key,"enabled") == False:
+                style = "off"
+            stream.write("<tr>")
+            # stream.write("<td>" + str(key) + "</td>")
+            stream.write("<td class="+style+" style='width:50%;padding-left:"+str(depth*25)+"'>" + str(stack.getProperty(key,"label")) + "</td>")
+            #stream.write("<td>" + str(stack.getProperty(key,"type")) + "</td>")
+            stream.write("<td class="+style+" style='width:200px;text-align:right'>" + str(stack.getProperty(key,"value")) + "</td>")
+            stream.write("<td class="+style+" >" + str(stack.getProperty(key,"unit")) + "</td>")
+            #stream.write("<td>" + str(stack.getProperty(key,"comments")) + "</td>")
+            stream.write("</tr>")
+
+        #look for children
+        if len(stack.getSettingDefinition(key).children) > 0:
+            for i in stack.getSettingDefinition(key).children:       
+                self._doTree(stack,i.key,stream,depth+1)
+                    
+                   
