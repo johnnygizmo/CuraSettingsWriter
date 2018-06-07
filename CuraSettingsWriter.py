250ae582a9ed63a70887f6da166eaa81d54e8a4d
Johnny Matthews <johnny.matthews@gmail.com>
2018-06-07 08:57:41 -0500

    Add Toggle

--- CuraSettingsWriter.py
+++ CuraSettingsWriter.py
@@ -12,6 +12,7 @@ class CuraSettingsWriter(WorkspaceWriter):
         stream.write("<style>")
         stream.write(" .category { font-size:1.5em; } ")
         stream.write(" .off { background-color:grey; } ")
+        stream.write(" .valueCol { width:200px;text-align:right }")
         stream.write("</style>")
         
         application = Application.getInstance()
@@ -19,6 +20,24 @@ class CuraSettingsWriter(WorkspaceWriter):
         stack = application.getGlobalContainerStack()
 
         global_stack = machine_manager.activeMachine
+
+
+        #jquery = open("plugins/plugins/CuraSettingsWriter/jquery-3.3.1.min.js","r", encoding="utf-8")
+        
+        stream.write("<script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>\n")
+        stream.write("""<script>
+                            $(document).ready(function(){
+                                    $("#enabled").on("click",toggleDisabled);
+
+                            });
+
+                            function toggleDisabled(){
+                                    $("tr.disabled").toggle();
+                            }
+                        </script>
+                        <h1>Cura Settings Export</h1>
+                        <button id='enabled'>Toggle Disabled</button><P>""")
+
 
         stream.write("<table width=50% border=1 cellpadding=3>")
 
@@ -54,19 +73,20 @@ class CuraSettingsWriter(WorkspaceWriter):
             stream.write("<tr>")
             stream.write("<td class=category colspan=3>" + str(stack.getProperty(key,"label")) + "</td>")
             #stream.write("<td class=category>" + str(key) + "</td>")
-            stream.write("</tr>")
+            stream.write("</tr>\n")
         else:
             style = "ok"    
             if stack.getProperty(key,"enabled") == False:
                 style = "off"
-            stream.write("<tr>")
-            # stream.write("<td>" + str(key) + "</td>")
+                stream.write("<tr class=disabled>")
+            else:
+                stream.write("<tr>")
             stream.write("<td class="+style+" style='width:50%;padding-left:"+str(depth*25)+"'>" + str(stack.getProperty(key,"label")) + "</td>")
-            #stream.write("<td>" + str(stack.getProperty(key,"type")) + "</td>")
-            stream.write("<td class="+style+" style='width:200px;text-align:right'>" + str(stack.getProperty(key,"value")) + "</td>")
+            stream.write("<td class='"+style+" valueCol'>" + str(stack.getProperty(key,"value")) + "</td>")
             stream.write("<td class="+style+" >" + str(stack.getProperty(key,"unit")) + "</td>")
             #stream.write("<td>" + str(stack.getProperty(key,"comments")) + "</td>")
-            stream.write("</tr>")
+            #stream.write("<td>" + str(stack.getProperty(key,"type")) + "</td>")
+            stream.write("</tr>\n")
 
         #look for children
         if len(stack.getSettingDefinition(key).children) > 0:
